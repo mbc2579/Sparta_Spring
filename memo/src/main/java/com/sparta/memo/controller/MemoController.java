6084af6c72e18022a2e102a96ffa3f622dd15fab
mbc2579 <mbc2579@naver.com>
2023-06-12 20:56:23 +0900

    sparta5(1) - memo Update, Delete

--- memo/src/main/java/com/sparta/memo/controller/MemoController.java
+++ memo/src/main/java/com/sparta/memo/controller/MemoController.java
@@ -22,7 +22,7 @@ public class MemoController {
         Memo memo = new Memo(requestDto);
 
         // Memo Max ID Check
-        Long maxId =memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
         memo.setId(maxId);
 
         // DB 저장
@@ -40,5 +40,32 @@ public class MemoController {
         List<MemoResponseDto> responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
 
         return responseList;
+    }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모 가져오기
+            Memo memo = memoList.get(id);
+
+            // memo 수정
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모를 삭제
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
     }
 }
