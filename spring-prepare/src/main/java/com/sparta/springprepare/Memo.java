64fc470589cfc0aaaa4e788d0a74455e504d7317
mbc2579 <mbc2579@naver.com>
2023-06-12 19:36:11 +0900

    sparta5(1)

---
+++ spring-prepare/src/main/java/com/sparta/springprepare/Memo.java
@@ -0,0 +1,19 @@
+package com.sparta.springprepare;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Memo {
+    private String username;
+    private String contents;
+}
+
+class Main {
+    public static void main(String[] args) {
+        Memo memo = new Memo();
+        memo.setUsername("KGJ");
+        System.out.println(memo.getUsername());
+    }
+}
