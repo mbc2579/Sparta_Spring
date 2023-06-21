1d378aea1f3f23282e3ab4044a22626c4cc16abb
mbc2579 <mbc2579@naver.com>
2023-06-21 15:29:07 +0900

    @OneToMany 단방향 및 조회 테스트

--- jpa-advance/src/test/java/com/sparta/jpaadvance/relation/OneToOneTest.java
+++ jpa-advance/src/test/java/com/sparta/jpaadvance/relation/OneToOneTest.java
@@ -31,7 +31,7 @@ public class OneToOneTest {
         Food food = new Food();
         food.setName("후라이드 치킨");
         food.setPrice(15000);
-        food.setUser(user); // 외래 키(연관 관계) 설정
+//        food.setUser(user); // 외래 키(연관 관계) 설정
 
         userRepository.save(user);
         foodRepository.save(food);
@@ -84,7 +84,7 @@ public class OneToOneTest {
         Food food = new Food();
         food.setName("고구마 피자");
         food.setPrice(30000);
-        food.setUser(user); // 외래 키(연관 관계) 설정
+//        food.setUser(user); // 외래 키(연관 관계) 설정
 
         userRepository.save(user);
         foodRepository.save(food);
@@ -98,15 +98,15 @@ public class OneToOneTest {
         System.out.println("food.getName() = " + food.getName());
 
         // 음식을 주문한 고객 정보 조회
-        System.out.println("food.getUser().getName() = " + food.getUser().getName());
+//        System.out.println("food.getUser().getName() = " + food.getUser().getName());
     }
 
     @Test
     @DisplayName("1대1 조회 : User 기준 food 정보 조회")
     void test6() {
-        User user = userRepository.findById(1L).orElseThrow(NullPointerException::new);
+//        User user = userRepository.findById(1L).orElseThrow(NullPointerException::new);
         // 고객 정보 조회
-        System.out.println("user.getName() = " + user.getName());
+//        System.out.println("user.getName() = " + user.getName());
 
         // 해당 고객이 주문한 음식 정보 조회
 //        Food food = user.getFood();
